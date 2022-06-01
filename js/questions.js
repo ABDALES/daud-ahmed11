f4537693f164681a2f9c74a91e234703c363bdb0
ABDALES <105119772+ABDALES@users.noreply.github.com>
2022-06-01 11:04:06 -0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "where city university is located?",
+    answer: "mugdisho",
+    options: [
+      "CEELBUUR",
+      "mugdisho",
+      "BOSASO",
+      "HARAGAYSA"
+    ]
+  },
+    {
+    numb: 2,
+    question: "when city university was established?",
+    answer: "2012",
+    options: [
+      "2012",
+      "2011",
+      "2017",
+      "2013"
+    ]
+  },
+    {
+    numb: 3,
+    question: "when CU classes were started?",
+    answer: "2013",
+    options: [
+      "2016",
+      "2010",
+      "2014",
+      "2013"
+    ]
+  },
+    {
+    numb: 4,
+    question: "the language of instruction at CU is",
+    answer: "English",
+    options: [
+      "arabic",
+      "somali",
+      "English",
+      "spanish"
+    ]
+  },
+    {
+    numb: 5,
+    question: "two more campuses of CU were opened in",
+    answer: "2014",
+    options: [
+      "2014",
+      "2015",
+      "2011",
+      "2019"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
